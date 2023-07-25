d9c1ec42346ce4d80e254712099bf7f85e177b03
Harrison045 <harrisonprince324@gmail.com>
2023-07-25 14:50:22 +0000

    done

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import profile from "./assets/profile.png";
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <Image source={profile} style={style.image} />
+      <View style={style.detailContainer}>
+        <Text style={style.label}>Name</Text>
+        <Text style={[style.label ,style.info]}>Tony Start</Text>
+      </View>
+      <View style={style.detailContainer}>
+        <Text style={style.label}>Email</Text>
+        <Text style={[style.label ,style.info]}>me@gmail.com</Text>
+      </View>
+      <View style={style.detailContainer}>
+        <Text style={style.label}>Gender</Text>
+        <Text style={[style.label ,style.info]}>She Male</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginHorizontal:5,
   },
+  image: {
+    marginTop:100,
+    width: 150,
+    height: 150,
+    marginBottom: 30,
+    alignSelf:"center",
+    borderRadius:75
+  },
+  detailContainer:{
+    flexDirection:"row",
+    marginVertical:3
+  },
+  label:{
+    borderColor:"black",
+    borderWidth:1,
+    fontSize:20,
+    flex:3,
+    paddingHorizontal: 5
+  },
+  info:{
+    flex:7
+  }
 });
+
+export default App;
